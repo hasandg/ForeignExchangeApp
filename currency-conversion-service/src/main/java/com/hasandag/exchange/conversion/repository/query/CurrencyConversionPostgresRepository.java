ad46a5d048a87ae72ac956f9af01fae70b0c330e
hasandg <hasan.dag@nar.com.tr>
2025-06-11 15:28:06 +0300

    spring-data-jpa-specifications-added

--- currency-conversion-service/src/main/java/com/hasandag/exchange/conversion/repository/query/CurrencyConversionPostgresRepository.java
+++ currency-conversion-service/src/main/java/com/hasandag/exchange/conversion/repository/query/CurrencyConversionPostgresRepository.java
@@ -16,10 +16,5 @@ public interface CurrencyConversionPostgresRepository extends JpaRepository<Curr
     Optional<CurrencyConversionEntity> findByTransactionId(String transactionId);
     
     boolean existsByTransactionId(String transactionId);
-    
-    Page<CurrencyConversionEntity> findByTimestampBetweenOrderByTimestampDesc(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
-    
-    Page<CurrencyConversionEntity> findByTimestampGreaterThanEqualOrderByTimestampDesc(LocalDateTime startDate, Pageable pageable);
-    
-    Page<CurrencyConversionEntity> findByTimestampLessThanEqualOrderByTimestampDesc(LocalDateTime endDate, Pageable pageable);
-} 
+
+}
